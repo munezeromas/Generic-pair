db0994c9189ca6faadf226a1694dc7cea14b6f26
MUNEZERO Aline <166438999+munezeromas@users.noreply.github.com>
2025-04-27 18:33:45 +0200

    Updated Pair.java

--- src/main/java/org/example/Pair.java
+++ src/main/java/org/example/Pair.java
@@ -1,4 +1,45 @@
 package org.example;
 
-public class Pair {
+public class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    public Pair(K key, V value) {
+        if (key == null) {
+            throw new IllegalArgumentException("Key cannot be null.");
+        }
+        this.key = key;
+        this.value = value;
+    }
+
+    public static <K, V> Pair<K, V> create(K key, V value) {
+        return new Pair<>(key, value);
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return "(Key: " + key + ", Value: " + value + ")";
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Pair<?, ?> pair = (Pair<?, ?>) obj;
+        return key.equals(pair.key) && (value == null ? pair.value == null : value.equals(pair.value));
+    }
+
+    public int hashCode() {
+        int result = key.hashCode();
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
 }
