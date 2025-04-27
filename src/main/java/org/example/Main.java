fd526edced83523bed6d41fab6293a0a7494fca6
MUNEZERO Aline <166438999+munezeromas@users.noreply.github.com>
2025-04-27 18:34:42 +0200

    Updated Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Pair<String, Integer> pair1 = new Pair<>("Age", 25);
+        Pair<Integer, String> pair2 = new Pair<>(404, "Not Found");
 
+        System.out.println("Pair 1: " + pair1);
+        System.out.println("Pair 2: " + pair2);
     }
 }
